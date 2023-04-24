6385e19b09623326844b18a4af5446ea5b0c36ff
JohnJerickArrogancia <131625591+JohnJerickArrogancia@users.noreply.github.com>
2023-04-24 13:45:22 +0800

    adasdad

--- hangman.js
+++ hangman.js
@@ -1,73 +1,88 @@
-// Array of words to choose from
-const words = ["javascript", "html", "css", "jquery", "react"];
+// Set the starting player
+let currentPlayer = "x";
+// Set a flag to indicate whether the game has ended
+let gameEnded = false;
 
-// Choose a random word from the array
-let word = words[Math.floor(Math.random() * words.length)];
-
-// Array to hold the letters guessed by the user
-let guessedLetters = [];
-
-// Get the word container element from the HTML
-const wordContainer = document.getElementById("word-container");
-
-// Display underscores for each letter in the word
-for (let i = 0; i < word.length; i++) {
-    wordContainer.innerHTML += "<span>_</span>";
+// Function to check if three cells match
+function checkRowCol(cell1, cell2, cell3) {
+    return cell1 === cell2 && cell2 === cell3 && cell1 !== "";
 }
 
-// Get the guess input and button elements from the HTML
-const guessInput = document.getElementById("guess-input");
-const guessButton = document.getElementById("guess-button");
-
-// Get the result container element from the HTML
-const resultContainer = document.getElementById("result-container");
-
-// Function to check if a letter has already been guessed
-function alreadyGuessed(letter) {
-    return guessedLetters.indexOf(letter) !== -1;
-}
-
-// Function to update the display of the word
-function updateWordDisplay() {
-    let display = "";
-    for (let i = 0; i < word.length; i++) {
-        if (alreadyGuessed(word[i])) {
-            display += word[i];
-        } else {
-            display += "_";
+// Function to check if the game has been won
+function checkWin() {
+    const cells = document.getElementsByClassName("cell");
+    // Check for horizontal wins
+    for (let i = 0; i < 7; i += 3) {
+        if (checkRowCol(cells[i].innerHTML, cells[i + 1].innerHTML, cells[i + 2].innerHTML)) {
+            return true;
         }
     }
-    wordContainer.innerHTML = display;
-}
+    // Check for vertical wins
+    for (let i = 0; i < 3; i++) {
+        if (checkRowCol(cells[i].innerHTML, cells[i + 3].innerHTML, cells[i + 6].innerHTML)) {
+            return true;
+        }
+    }
+    // Check for diagonal wins
+    if (checkRowCol(cells[0].innerHTML, cells[4].innerHTML, cells[8].innerHTML)
+        || checkRowCol(cells[2].innerHTML, cells[4].innerHTML, cells[6].innerHTML)) {
+            return true;
+        }
+    }
+    // No win
+    return false;
 
-// Function to check if the user has won
-function checkWin() {
-    for (let i = 0; i < word.length; i++) {
-        if (!alreadyGuessed(word[i])) {
+
+// Function to check if the game is a tie
+function checkTie() {
+    const cells = document.getElementsByClassName("cell");
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].innerHTML === "") {
             return false;
         }
     }
     return true;
 }
 
-// Function to handle a guess from the user
-function handleGuess() {
-    let guess = guessInput.value.toLowerCase();
+// Function to display a message to the player(s)
+function showMessage(message) {
+    document.getElementById("message").innerHTML = message;
+}
 
-    // Clear the guess input
-    guessInput.value = "";
-
-    // Check if the letter has already been guessed
-    if (alreadyGuessed(guess)) {
-        resultContainer.innerHTML = "You already guessed that letter!";
+// Function to handle a cell being clicked
+function cellClicked() {
+    // If the game has ended, do nothing
+    if (gameEnded) {
         return;
     }
-
-    guessedLetters.push(guess);
-
-    // Update the display of the word
-    updateWordDisplay();
-
-    // Check if the user has won
+    // If the cell is already occupied, do nothing
+    if (this.innerHTML !== "") {
+        return;
+    }
+    // Place the current player's mark on the cell
+    this.innerHTML = currentPlayer;
+    // Check for a win or tie
     if (checkWin()) {
-        resultContainer.innerHTML = "Congratulations! You won!";
+        showMessage(`Player ${currentPlayer} wins!`);
+        gameEnded = true;
+        return;
+    }
+    if (checkTie()) {
+        showMessage("Tie game!");
+        gameEnded = true;
+        return;
+    }
+    // Switch to the other player
+    currentPlayer = currentPlayer === "x" ? "o" : "x";
+    // Display the current player's mark in the message
+    showMessage(`Player ${currentPlayer}'s turn`);
+}
+
+// Add a click listener to each cell
+const cells = document.getElementsByClassName("cell");
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", cellClicked);
+}
+
+// Display the starting message
+showMessage(`Player ${currentPlayer}'s turn`);
